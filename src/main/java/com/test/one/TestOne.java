ae767551521cc8c7be43ad8254410b17fb823dfa
zyl <??>
2020-11-16 07:02:27 +0800

    first commit

---
+++ src/main/java/com/test/one/TestOne.java
@@ -0,0 +1,12 @@
+package com.test.one;
+
+public class TestOne {
+
+    public Integer testOne;
+
+    private String testTwo;
+
+    public TestOne() {
+        System.out.println("恭喜你，maven坐标构建成功");
+    }
+}
